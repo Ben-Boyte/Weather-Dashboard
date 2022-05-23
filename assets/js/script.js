a23840d74318d73b0d52b25e78c07b42a80920c5
Ben-Boyte <zissette1@gmail.com>
2022-05-23 14:05:54 -0500

    save search working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,56 @@
-var cityInput = document.getElementById("city-input");
-var cityName = document.getElementById("city");
-var searchEl = document.getElementById("search-button");
-var currentPic = document.getElementById("current-pic");
-var weatherToday = document.getElementById("weather-today")
-var tempEl = document.getElementById("temperature");
-var humidEl = document.getElementById("humidity");
-var windEl = document.getElementById("wind-speed");
-var uvEl = document.getElementById("UV-index");
-var historyEl = document.getElementById("history");
-var apiKey = "23a2969256bbc0a60e21d67c4fcf10ab"
+    var enterCity = document.getElementById("enter-city");
+    var cityName = document.getElementById("city");
+    var search = document.getElementById("search-button");
+    var currentPic = document.getElementById("current-pic");
+    var weatherToday = document.getElementById("weather-today");
+    var fivedayForecast = document.getElementById("fiveday-forecast");
+    var currentTemp = document.getElementById("temperature");
+    var currentHumidity = document.getElementById("humidity");
+    var currentWind = document.getElementById("wind-speed");
+    var currentUV = document.getElementById("UV-index");
+    var historyEl = document.getElementById("history");
+    var apiKey = "23a2969256bbc0a60e21d67c4fcf10ab"
 
-
-
- function weather(cityName) {
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-    axios.get(queryURL)
-    .then(function (response) {
+    search.addEventListener("click", function () {
+        var searchResult = enterCity.value;
+        weather(searchResult);
+        searchHistory.push(searchResult);
+        localStorage.setItem("search", JSON.stringify(searchHistory));
+        saveSearch();
     })
 
- }
-
- function saveSearch() {
-    
-   let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-
-   historyEl.innerHTML = "";
-
-   for (let i = 0; i < searchHistory.length; i++) {
-       const historyItem = document.createElement("input");
-       historyItem.setAttribute("type", "text");
-       historyItem.setAttribute("readonly", true);
-       historyItem.setAttribute("class", "form-control d-block bg-white");
-       historyItem.setAttribute("value", searchHistory[i]);
-       historyItem.addEventListener("click", function () {
-           weather(historyItem.value);
-       })
-       historyEl.append(historyItem);
-   }
+function math(K) {
+    return Math.floor((K - 273.15) * 1.8 + 32);
 }
+
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+function saveSearch() {
+    historyEl.innerHTML = "";
+    for (let i = 0; i < searchHistory.length; i++) {
+        var historyResult = document.createElement("input");
+        historyResult.setAttribute("type", "text");
+        historyResult.setAttribute("readonly", true);
+        historyResult.setAttribute("class", "form-control d-block bg-white");
+        historyResult.setAttribute("value", searchHistory[i]);
+        historyResult.addEventListener("click", function () {
+            weather(historyResult.value);
+        })
+        historyEl.append(historyResult);
+    }
+}
+
+saveSearch();
+if (searchHistory.length > 0) {
+    weather(searchHistory[searchHistory.length - 1]);
+}
+
+
+    function weather(cityName) {
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+       
+    }
+
+
+        
+weather();
